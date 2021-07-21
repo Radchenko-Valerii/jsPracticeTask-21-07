caac46c1eed8c3de642114bd11ee10f712b8d9b8
Valerii Radchenko <valeravivaz@gmail.com>
2021-07-21 14:28:53 +0300

    add first tree task

---
+++ assets/js/index.js
@@ -0,0 +1,24 @@
+// FIRST TASK
+
+
+// for(let i=25; i >= 0; i--){
+//   console.log(i);
+// }
+
+
+// SECOND TASK
+
+// for(let i=10; i <= 50; i++){
+//   if (i % 5 === 0){
+//     console.log(i);
+//   }
+// }
+
+
+// THIRD TASK
+
+// let j=0;
+// for(let i=1; i <= 100; i++){
+//   j +=i;
+// }
+// console.log(j);
